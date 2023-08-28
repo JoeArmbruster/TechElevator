fccc91ad113a78ef9fbb80cfa3298ac9df3e3543
Casey McCullough <casey.mccullough@techelevator.com>
2023-08-28 15:34:52 -0600

    added StaticMethodPractice and Solutions

---
+++ java/module-1/week-3/Instructor_Material/static-methods/src/main/java/StaticMethodPracticeSolutions.java
@@ -0,0 +1,38 @@
+public class StaticMethodPracticeSolutions {
+    public static void main(String[] args) {
+        /*
+            Method calls will take place here, in main().
+        */
+
+
+    }
+
+    /*
+        Method definitions are below
+    */
+
+    // FORMAT:
+    // public static return-type name-of-method (parameter or parameters)
+
+    // int tripleIt(int num)
+    // return 3 times the number n
+
+    // boolean isEvenAndPositive (int num)
+    // returns true if the number is even and positive,
+    // false otherwise
+
+    // printNTimes(String s, int numTimes)
+    // output to the console the string s numTimes times
+    // output should be on a single line
+
+    // String intersperseCharacter (String s, char c)
+    // return a string that has every character from s
+    // interspersed with the character c
+
+    // int timesFound (int key, int[] values)
+    // return the number of times that the key value is found in values
+
+    // void changeValuesToNines (int[] values)
+    // changes every value in the array to a 9
+
+}
