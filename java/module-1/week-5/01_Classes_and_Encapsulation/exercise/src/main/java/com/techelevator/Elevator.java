a36e3b9074a2b798c471f3699cf225a5568c72ac
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-09-14 22:04:49 -0400

    finished exercises week 5

--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,53 @@ package com.techelevator;
 
 public class Elevator {
 
+    private int currentFloor;
+    private int numberOfFloors;
+    private boolean doorOpen;
+
+    public Elevator(int numberOfFloors){
+        this.numberOfFloors = numberOfFloors;
+        this.currentFloor = 1;
+    }
+
+    public void openDoor(){
+        if(!doorOpen){
+            doorOpen = true;
+        }
+    }
+
+    public void closeDoor(){
+        if (doorOpen){
+            doorOpen = false;
+        }
+    }
+
+    public void goUp(int desiredFloor) {
+        if (!doorOpen){
+            if (desiredFloor > currentFloor && desiredFloor <= numberOfFloors){
+                currentFloor = desiredFloor;
+            }
+        }
+    }
+
+    public void goDown(int desiresFloor){
+        if (!doorOpen) {
+            if (desiresFloor < currentFloor && desiresFloor >= 1){
+                currentFloor = desiresFloor;
+            }
+        }
+    }
+
+    public int getCurrentFloor(){
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors(){
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen(){
+        return doorOpen;
+    }
+
 }
--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,44 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
 
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+        this.earnedMarks = 0;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public void setEarnedMarks(int earnedMarks){
+        this.earnedMarks = earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade() {
+        double percentage = (earnedMarks * 100.0) / possibleMarks;
+        if(percentage >= 90) {
+            return "A";
+        } else if (percentage >= 80) {
+            return "B";
+        } else if (percentage >= 70) {
+            return "C";
+        } else if (percentage >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
 }
