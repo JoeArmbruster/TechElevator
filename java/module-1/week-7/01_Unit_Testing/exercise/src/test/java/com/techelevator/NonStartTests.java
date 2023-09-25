64691cd911a5dbd5a910cc8f7e7a73634290e188
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-09-24 21:37:46 -0400

    Unit_Testing Exercises commplete - added a few more tests to NonStartTests

--- java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/NonStartTests.java
+++ java/module-1/week-7/01_Unit_Testing/exercise/src/test/java/com/techelevator/NonStartTests.java
@@ -6,23 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NonStartTests {
 
     @Test
-    public void omitFirstCharCaps(){
+    public void testOmitFirstCharCaps(){
         NonStart nonStart = new NonStart();
         String result = nonStart.getPartialString("Hello", "There");
         assertEquals("ellohere", result);
     }
 
     @Test
-    public void omitFirstCharLowercase(){
+    public void testOmitFirstCharLowercase(){
         NonStart nonStart = new NonStart();
         String result = nonStart.getPartialString("java", "code");
         assertEquals("avaode", result);
     }
 
     @Test
-    public void omitFirstCharLastLetterCaps(){
+    public void testOmitFirstCharLastLetterCaps(){
         NonStart nonStart = new NonStart();
         String result = nonStart.getPartialString("shotL", "java");
         assertEquals("hotLava", result);
     }
+
+    @Test
+    public void testFirstEmpty(){
+        NonStart nonStart = new NonStart();
+        String result = nonStart.getPartialString("", "There");
+        assertEquals("here", result);
+    }
+
+    @Test
+    public void testSecondEmpty(){
+        NonStart nonStart = new NonStart();
+        String result = nonStart.getPartialString("Hello", "");
+        assertEquals("ello", result);
+    }
+
+    @Test
+    public void testBothEmpty(){
+        NonStart nonStart = new NonStart();
+        String result = nonStart.getPartialString("", "");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testBothOneCharacter(){
+        NonStart nonStart = new NonStart();
+        String result = nonStart.getPartialString("a", "b");
+        assertEquals("", result);
+    }
+
 }
