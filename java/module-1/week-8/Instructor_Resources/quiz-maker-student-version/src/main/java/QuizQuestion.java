ebd35621f6289a4184849a549131bb6cc4f0a2bf
Casey McCullough <casey.mccullough@techelevator.com>
2023-10-02 08:41:54 -0600

    added instructor resources folder with QuizMaker project

---
+++ java/module-1/week-8/Instructor_Resources/quiz-maker-student-version/src/main/java/QuizQuestion.java
@@ -0,0 +1,45 @@
+public class QuizQuestion {
+
+    /*
+    Instance variables:
+        question (a String)
+        answers (an array of Strings)
+        correctAnswer (a String)
+
+
+    Constructor (do this last)
+
+        QuizQuestion (String lineOfText)
+
+        Parameter lineOfText has the following format:
+
+            What color is the sky?|yellow|blue*|green|red
+
+    Getter methods for
+        question
+        answers
+
+    boolean isCorrectAnswer (String)
+
+     */
+
+    /*
+        The provided toString() method will allow you to confirm that your
+        constructor works correctly.
+     */
+
+
+//    @Override
+//    public String toString(){
+//        String questionInfo = "";
+//        questionInfo += "Question: " + question;
+//        questionInfo += "\nPossible answers: ";
+//        for (String answer : answers) {
+//            questionInfo += "\n" + answer;
+//        }
+//        questionInfo += "\nCorrect answer: " + correctAnswer;
+//
+//        return questionInfo;
+//    }
+
+}
