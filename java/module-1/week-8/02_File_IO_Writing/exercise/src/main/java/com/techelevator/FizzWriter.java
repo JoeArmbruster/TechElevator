4c49fc6109e1ef7216ebb5a8fa6eca45fd6f10cb
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-10-04 23:43:44 -0400

    week 8 Fizz Buzz exercise

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,21 +1,38 @@
 package com.techelevator;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FizzWriter {
 
-	// Use this scanner for all user input. Don't create additional Scanners with System.in
-	private final Scanner userInput = new Scanner(System.in);
+    // Use this scanner for all user input. Don't create additional Scanners with System.in
+    private final Scanner userInput = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		FizzWriter fizzWriter = new FizzWriter();
-		fizzWriter.run();
-	}
+    public static void main(String[] args) {
+        FizzWriter fizzWriter = new FizzWriter();
+        fizzWriter.run();
+    }
 
-	public void run() {
-		/* Your code goes here */
+    public void run() {
+        /* Your code goes here */
+        System.out.println("Enter the destination file path: ");
+        String destinationFile = userInput.nextLine();
 
-
-	}
-
+        try (FileWriter writer = new FileWriter(destinationFile)) {
+            for (int i = 1; i <= 300; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    writer.write("FizzBuzz\n");
+                } else if (i % 3 == 0) {
+                    writer.write("Fizz\n");
+                } else if (i % 5 == 0) {
+                    writer.write("Buzz\n");
+                } else {
+                    writer.write(i + "\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
