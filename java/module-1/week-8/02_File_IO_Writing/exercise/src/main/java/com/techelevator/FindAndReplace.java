0b1c8b2cafed629d95218ea63f22d5a7fb66f1e2
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-10-04 23:26:25 -0400

    week 8 File Writing exercise

--- java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ java/module-1/week-8/02_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -13,9 +16,34 @@ public class FindAndReplace {
     }
 
     public void run() {
-        /* Your code goes here */
+        System.out.println("What is the search word? ");
+        String searchWord = userInput.nextLine();
+        System.out.println("What is the replacement word? ");
+        String replacementWord = userInput.nextLine();
+        System.out.println("What is the source file? ");
+        String sourceFile = userInput.nextLine();
+        System.out.println("What is the destination file? ");
+        String destinationFile = userInput.nextLine();
 
+        File search = new File(sourceFile);
+        File output = new File(destinationFile);
 
+        try {
+            Scanner scanner = new Scanner(search);
+            PrintWriter printWriter = new PrintWriter(output);
+
+            while (scanner.hasNextLine()) {
+                String lineOfInput = scanner.nextLine();
+                lineOfInput = lineOfInput.replace(searchWord, replacementWord);
+                printWriter.println(lineOfInput);
+            }
+
+            scanner.close();
+            printWriter.close();
+
+            System.out.println("Replacement completed successfully.");
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
     }
-
 }
