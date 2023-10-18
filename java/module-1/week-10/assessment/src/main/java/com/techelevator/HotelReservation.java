7155adbde48be482686eaa77561eb20e029ef068
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-10-18 19:50:45 -0400

    Assessment Part 2 complete!

--- java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
+++ java/module-1/week-10/assessment/src/main/java/com/techelevator/HotelReservation.java
@@ -1,8 +1,68 @@
 package com.techelevator;
 
 public class HotelReservation {
-    /*
-    Fill in the class details here...
-     */
 
+    private String name;
+    private int numberOfNights;
+    private int nightlyRate;
+    private int estimatedTotal;
+
+    private static final int CLEANING_FEE = 25;
+    private static final int MINIBAR_FEE = 15;
+
+    public HotelReservation(String name, int numberOfNights, int nightlyRate) {
+        this.name = name;
+        this.numberOfNights = numberOfNights;
+        this.nightlyRate = nightlyRate;
+        this.estimatedTotal = numberOfNights * nightlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+        this.estimatedTotal = numberOfNights * nightlyRate;
+    }
+
+    public int getNightlyRate() {
+        return nightlyRate;
+    }
+
+    public void setNightlyRate(int nightlyRate) {
+        this.nightlyRate = nightlyRate;
+        this.estimatedTotal = numberOfNights * nightlyRate;
+    }
+
+    public int getEstimatedTotal() {
+        return estimatedTotal;
+    }
+
+    public int getActualTotal (boolean requiresCleaning, boolean usedMinibar){
+        int actualTotal = estimatedTotal;
+
+        if (requiresCleaning && usedMinibar){
+            actualTotal += MINIBAR_FEE;
+            actualTotal += 2 * CLEANING_FEE;
+        } else if (requiresCleaning) {
+            actualTotal += CLEANING_FEE;
+        } else if (usedMinibar) {
+            actualTotal += MINIBAR_FEE;
+        }
+        return actualTotal;
+    }
+
+    @Override
+    public String toString() {
+        return name + ":" + estimatedTotal;
+    }
 }
