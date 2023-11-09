0e81df9b01f29a410a18f3f199af6adc876a5de0
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-11-09 18:47:18 -0500

    add insert_update_delete exercises

--- java/module-2/week-2/02_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_update_old_profiles.sql
+++ java/module-2/week-2/02_INSERT_UPDATE_and_DELETE/exercise/Exercises/10_update_old_profiles.sql
@@ -1,4 +1,6 @@
 -- 10. For all people born before 1900 whose profile_path does NOT end in ".jpg", set their
 --     home_page to "No image" and their profile_path to NULL (64 rows)
 
-
+UPDATE person
+SET home_page = 'No image', profile_path = NULL
+WHERE EXTRACT (YEAR FROM birthday) < 1900 AND NOT profile_path LIKE '%.jpg';
