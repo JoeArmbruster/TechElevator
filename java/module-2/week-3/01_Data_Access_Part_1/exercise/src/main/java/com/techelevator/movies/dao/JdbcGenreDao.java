eca4d8e6605ec54f78f34c594fd1f6f8209d1c12
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-11-15 17:37:50 -0500

    no errors x2!

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcGenreDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcGenreDao.java
@@ -2,6 +2,7 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Genre;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,16 +18,47 @@ public class JdbcGenreDao implements GenreDao {
 
     @Override
     public List<Genre> getGenres() {
-        return null;
+        List<Genre> genres = new ArrayList<>();
+        String sql = "SELECT genre_id, genre_name FROM genre;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while(results.next()){
+            Genre genre = mapRowToGenre(results);
+            genres.add(genre);
+        }
+        return genres;
     }
 
     @Override
     public Genre getGenreById(int id) {
-        return new Genre(-1, "Not implemented yet");
+        Genre genre = null;
+        String sql = "SELECT genre_id, genre_name FROM genre WHERE genre_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+        if (results.next()){
+            genre = mapRowToGenre(results);
+        }
+        return genre;
     }
 
     @Override
     public List<Genre> getGenresByName(String name, boolean useWildCard) {
-        return null;
+        List<Genre> genres = new ArrayList<>();
+        if (useWildCard){
+            name = "%" + name + "%";
+        }
+        String sql = "SELECT genre_id, genre_name FROM genre WHERE genre_name ILIKE ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, name);
+
+        while(results.next()){
+            genres.add(mapRowToGenre(results));
+        }
+        return genres;
     }
+
+    public Genre mapRowToGenre(SqlRowSet results){
+        Genre genre = new Genre();
+        genre.setId(results.getInt("genre_id"));
+        genre.setName(results.getString("genre_name"));
+        return genre;
+    }
+
 }
