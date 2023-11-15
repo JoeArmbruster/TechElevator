7e181874b568e93fbb3645d59ed0eb6006aefdc0
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-11-15 17:13:10 -0500

    testing something

--- java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ java/module-2/week-3/01_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -2,6 +2,7 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Collection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,16 +18,36 @@ public class JdbcCollectionDao implements CollectionDao{
 
     @Override
     public List<Collection> getCollections() {
-        return null;
+        List<Collection> collections = new ArrayList<>();
+        String sql = "SELECT collection_id, collection_name FROM collection;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()){
+            Collection collection = mapRowToCollection(results);
+            collections.add(collection);
+        }
+        return collections;
     }
 
     @Override
     public Collection getCollectionById(int id) {
-        return new Collection(-1, "Not implemented yet");
+        Collection collection = new Collection();
+        String sql = "SELECT collection_id, collection_name FROM collection WHERE collection_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+        if (results.next()){
+            collection = mapRowToCollection(results);
+        }
+        return collection;
     }
 
     @Override
     public List<Collection> getCollectionsByName(String name, boolean useWildCard) {
         return null;
+    }
+
+    public Collection mapRowToCollection(SqlRowSet results){
+        Collection collection = new Collection();
+        collection.setId(results.getInt("collection_id"));
+        collection.setName(results.getString("collection_name"));
+        return collection;
     }
 }
