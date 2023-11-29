a67f654afd2e194d00cd5fe5afe299e4d70145ba
Joseph Armbruster <newmotorsjoe25@gmail.com>
2023-11-29 18:54:17 -0500

    Working on mid-module project

--- java/module-2/week-5/mid-module-project/src/main/java/com/techelevator/ssgeek/dao/JdbcLineItemDao.java
+++ java/module-2/week-5/mid-module-project/src/main/java/com/techelevator/ssgeek/dao/JdbcLineItemDao.java
@@ -1,20 +1,52 @@
 package com.techelevator.ssgeek.dao;
 
+import com.techelevator.ssgeek.exception.DaoException;
 import com.techelevator.ssgeek.model.LineItem;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
-public class JdbcLineItemDao implements LineItemDao{
+public class JdbcLineItemDao implements LineItemDao {
 
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
-    public JdbcLineItemDao(DataSource dataSource){
-        this.dataSource = dataSource;
+    public JdbcLineItemDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate((dataSource));
     }
 
     @Override
     public List<LineItem> getLineItemsBySaleId(int saleId) {
-        return null;
+        List<LineItem> lineItems = new ArrayList<>();
+        String sql = "SELECT line_item.*, product.name AS product_name, product.price " +
+                "FROM line_item " +
+                "JOIN product ON line_item.product_id = product.product_id " +
+                "WHERE sale_id = ? " +
+                "ORDER BY line_item.line_item_id;";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, saleId);
+            while (results.next()) {
+                LineItem lineItem = mapRowToLineItem(results);
+                lineItems.add(lineItem);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return lineItems;
+    }
+
+    private LineItem mapRowToLineItem(SqlRowSet results) {
+        LineItem lineItem = new LineItem();
+        lineItem.setLineItemId(results.getInt("line_item_id"));
+        lineItem.setSaleId(results.getInt("sale_id"));
+        lineItem.setProductId(results.getInt("product_id"));
+        lineItem.setQuantity(results.getInt("quantity"));
+        lineItem.setProductName(results.getString("product)name"));
+        lineItem.setPrice(results.getBigDecimal("price"));
+        return lineItem;
     }
 }
--- java/module-2/week-5/mid-module-project/src/test/java/com/techelevator/ssgeek/dao/JdbcLineItemDaoTests.java
+++ java/module-2/week-5/mid-module-project/src/test/java/com/techelevator/ssgeek/dao/JdbcLineItemDaoTests.java
@@ -1,4 +1,47 @@
 package com.techelevator.ssgeek.dao;
 
-public class JdbcLineItemDaoTests extends BaseDaoTests{
+import com.techelevator.ssgeek.model.LineItem;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.sound.sampled.Line;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class JdbcLineItemDaoTests extends BaseDaoTests {
+
+    private static final LineItem TEST_LINE_ITEM_1 = new LineItem(1, 1, 1, 1, "Product 1", new BigDecimal("9.99"));
+    private static final LineItem TEST_LINE_ITEM_2 = new LineItem(2, 1, 2, 1, "Product 2", new BigDecimal("19.00"));
+    private static final LineItem TEST_LINE_ITEM_3 = new LineItem(3, 1, 4, 1, "Product 4", new BigDecimal("0.99"));
+    private static final LineItem TEST_LINE_ITEM_4 = new LineItem(4, 2, 3, 10, "Product 4", new BigDecimal("0.99"));
+    private static final LineItem TEST_LINE_ITEM_5 = new LineItem(5, 2, 1, 10, "Product 1", new BigDecimal("9.99"));
+    private static final LineItem TEST_LINE_ITEM_6 = new LineItem(6, 3, 1, 100, "Product 1", new BigDecimal("9.99"));
+
+    private JdbcLineItemDao jdbcLineItemDao;
+
+    public void setup() {
+        jdbcLineItemDao = new JdbcLineItemDao(dataSource);
+    }
+
+    @Test
+    public void getLineItemsBySaleId_returns_correct_line_items() {
+        List<LineItem> lineItems = jdbcLineItemDao.getLineItemsBySaleId(1);
+
+        Assert.assertEquals(3, lineItems.size());
+        assertLineItemsMatch("Returned wrong or partial data", TEST_LINE_ITEM_1, lineItems.get(0));
+        assertLineItemsMatch("Returned wrong or partial data", TEST_LINE_ITEM_2, lineItems.get(1));
+        assertLineItemsMatch("Returned wrong or partial data", TEST_LINE_ITEM_3, lineItems.get(2));
+
+    }
+
+    private void assertLineItemsMatch(String message, LineItem expected, LineItem actual){
+        Assert.assertEquals(message, expected.getLineItemId(), actual.getLineItemId());
+        Assert.assertEquals(message, expected.getSaleId(), actual.getSaleId());
+        Assert.assertEquals(message, expected.getProductId(), actual.getProductId());
+        Assert.assertEquals(message, expected.getQuantity(), actual.getQuantity());
+        Assert.assertEquals(message, expected.getProductName(), actual.getProductName());
+        Assert.assertEquals(message, 0, expected.getPrice().compareTo(actual.getPrice()));
+
+    }
+
 }
