9dbf1d4144a492792d896d5ad90eeb45732c3328
Joseph Armbruster <newmotorsjoe25@gmail.com>
2024-03-12 17:54:58 -0400

    Mod 3 Week 7 Vue Router Exercise 5/6 complete

--- java/module-3/week-7/02_Vue_Router/exercise/src/App.vue
+++ java/module-3/week-7/02_Vue_Router/exercise/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="book-app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <span class="separator">|</span>
+      <router-link to="/myBooks">My Books</router-link>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-
-export default {
-  
-}
+export default {};
 </script>
 
 <style>
 #book-app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin: 60px;
+}
+
+.separator {
+  margin: 0 5px;
 }
 </style>
--- java/module-3/week-7/02_Vue_Router/exercise/src/router/index.js
+++ java/module-3/week-7/02_Vue_Router/exercise/src/router/index.js
@@ -1,6 +1,21 @@
 import { createRouter as _createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import MyBooksView from '../views/MyBooksView.vue';
+import NewBookView from '../views/NewBookView.vue';
 
 const routes = [
+  {
+    path: '/',
+    component: HomeView
+  },
+  {
+    path: '/myBooks',
+    component: MyBooksView
+  },
+  {
+    path: '/addBook',
+    component: NewBookView
+  }
 
 ];
 
