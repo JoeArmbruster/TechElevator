8167c2bce695c6e65ee02e9c71d4e54b82066b79
Joseph Armbruster <newmotorsjoe25@gmail.com>
2024-03-12 15:28:05 -0400

    Mod 3 Week 7 Vue Component Exercise

---
+++ java/module-3/week-7/01_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="new-book-form">
+    <form @submit.prevent="addBook">
+      <label for="title">Title:</label>
+      <input type="text" id="title" v-model="title" class="title-input" required>
+      <label for="author">Author:</label>
+      <input type="text" id="author" v-model="author" class="author-input" required>
+      <label for="isbn">ISBN:</label>
+      <input type="text" id="isbn" v-model="isbn" class="isbn-input" required>
+      <button type="submit">Add Book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      author: '',
+      isbn: ''
+    };
+  },
+  methods: {
+    addBook() {
+      const newBook = {
+        title: this.title,
+        author: this.author,
+        read: false,
+        isbn: this.isbn
+      };
+      this.$store.commit('ADD_BOOK', newBook);
+      this.title = '';
+      this.author = '';
+      this.isbn = '';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ java/module-3/week-7/Instructor_Materials/vue-week-2-boulder-bolt-step2-student-starter/src/views/RegistrationView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id = "registration">
+    <h1>Race Registration</h1>
+    <race-registration/>
+  </div>
+</template>
+
+<script>
+import RaceRegistration from '../components/RaceRegistration.vue';
+
+export default {
+  components: {
+    RaceRegistration,
+  }
+
+}
+
+</script>
