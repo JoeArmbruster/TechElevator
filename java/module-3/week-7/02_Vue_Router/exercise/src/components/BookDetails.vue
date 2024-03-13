b25b5b89189147a270b353d70c93fc9ba945ccc2
Joseph Armbruster <newmotorsjoe25@gmail.com>
2024-03-13 00:02:46 -0400

    Mod 3 Week 7 Vue Router Exercise

---
+++ java/module-3/week-7/02_Vue_Router/exercise/src/components/BookDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/myBooks">My Books</router-link>
+    </nav>
+    <h2>{{ book.title }}</h2>
+    <h3>{{ book.author }}</h3>
+    <img :src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-L.jpg'" alt="Book Cover" />
+    <p>Number of Pages: {{ book.numPages }}</p>
+    <a :href="book.moreInfoLink" target="_blank">More Information</a>
+    <button @click="markAsRead">{{ book.read ? 'Mark as Unread' : 'Mark as Read' }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    book: Object    
+  },
+  methods: {
+    markAsRead() {
+      this.$store.commit('SET_READ_STATUS', { book: this.book, value: !this.book.read });
+    }
+  }
+}
+</script>
+
+<style>
+.separator {
+  margin: 0 5px;
+}
+</style>
