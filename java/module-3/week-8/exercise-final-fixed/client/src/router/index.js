87982c319e814e00e9b405223f955648612b8d47
Casey McCullough <casey.mccullough@techelevator.com>
2024-03-21 08:46:18 -0600

    added fixed version of instructor solution for week-8 pair programming

---
+++ java/module-3/week-8/exercise-final-fixed/client/src/router/index.js
@@ -0,0 +1,51 @@
+import { createRouter as _createRouter, createWebHistory } from 'vue-router'
+
+import HotelsView from '../views/HotelsView.vue'
+import HotelDetailsView from '../views/HotelDetailsView.vue'
+import ReservationAddView from '../views/ReservationAddView.vue'
+import ReservationUpdateView from '../views/ReservationUpdateView.vue'
+import ReservationDeleteView from '../views/ReservationDeleteView.vue'
+
+/**
+ * The Vue Router is used to "direct" the browser to render a specific view component
+ * inside of App.vue depending on the URL.
+ */
+
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: HotelsView,
+    },
+    {
+      path: '/hotel/:id',
+      name: 'hotel',
+      component: HotelDetailsView,
+    },
+    // Note the parameter here (hotelId) needs to match 
+    // the parameter expected in ReservationAddView, line 117.
+    // THIS SOLUTION HAS BEEN CHANGED BELOW, :id to :hotelId
+    {
+      path: '/hotel/:hotelId/newreservation',
+      name: 'add-reservation',
+      component: ReservationAddView,
+    },
+
+    {
+      path: '/reservation/:id/update',
+      name: 'update-reservation',
+      component: ReservationUpdateView,
+    },
+    {
+      path: '/reservation/:id/delete',
+      name: 'delete-reservation',
+      component: ReservationDeleteView,
+    },
+  ];
+
+export function createRouter () {
+  return _createRouter({
+    history: createWebHistory(),
+    routes: routes
+  })
+}
