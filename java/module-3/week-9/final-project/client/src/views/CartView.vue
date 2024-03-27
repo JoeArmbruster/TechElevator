98a6d129228b78d422392cb719c37afc574b7dcd
Joseph Armbruster <newmotorsjoe25@gmail.com>
2024-03-27 11:43:37 -0400

    Mod 3 Final Project

--- java/module-3/week-9/final-project/client/src/views/CartView.vue
+++ java/module-3/week-9/final-project/client/src/views/CartView.vue
@@ -1,15 +1,64 @@
 <template>
-<div id="cart-view">
-  <h1>Cart View</h1>
-</div>
+  <div id="cart-view">
+    <h1>Shopping Cart</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Quantity</th>
+          <th>Product</th>
+          <th>Price</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+
+    <div id="cart-summary">
+      <div>
+        <span>Subtotal:</span>
+        <span>{{ formatCurrency(Subtotal) }}</span>
+      </div>
+      <div>
+        <span>Tax (6%):</span>
+        <span>{{ formatCurrency(tax) }}</span>
+      </div>
+      <div>
+        <span>Total:</span>
+        <span>{{ formatCurrency(total) }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      cartItems: [],
+    };
+  },
+  computed: {
+    subtotal() {
+      return this.cartItems.reduce(
+        (acc, item) => acc + item.quantity * item.price,
+        0
+      );
+    },
+    tax() {
+      return this.subtotal * 0.06;
+    },
+    total() {
+      return this.subtotal + this.tax;
+    },
+  },
+  methods: {
+    formatCurrency(amount) {
+      return amount.toFixed(2);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
